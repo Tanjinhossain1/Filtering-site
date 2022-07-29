205ed27f3c495dc917f956e2c493814440de536c
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-07-29 15:04:30 +0600

    product add done

---
+++ src/components/Cloth.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faStar } from '@fortawesome/free-solid-svg-icons';
+
+const Cloth = ({cloth}) => {
+    const {name,img,price,ratings} = cloth
+    return (
+      <div className='product'>
+      <img src={img} alt=""></img>
+      <div className='product-info'>
+          <p className='product-name'>{name}</p>
+          <p>Price: <span className='text-purple-600 font-bold'>${price}</span></p>
+
+          <p><small>Ratings: {ratings} <FontAwesomeIcon className='text-orange-600' icon={faStar} /></small></p>
+      </div>
+      <button className='btn-cart'>
+          <p className='btn-text'>Add to Cart</p>
+          <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+      </button>
+  </div>
+    );
+};
+
+export default Cloth;
--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Cloth from './Cloth';
 
 const Filtering = () => {
+    const allCategories = ["Men's Sneaker", "Men's Pants", "Men's Boot","Bag","Cap","Earphones","Bottle"]
+    const [cloths,setCloths] = useState([])
+    useEffect(()=>{
+        fetch('cloths.json')
+        .then(res=>res.json())
+        .then(data=>setCloths(data))
+    },[])
+    
     return (
         <div className=' bg-[#f8f7fd]'>
             <div className=' w-3/4 mx-auto'>
@@ -9,7 +18,42 @@ const Filtering = () => {
                     <h1 className='text-4xl font-semibold'>Search properties to rent</h1>
                     <input type="text" placeholder="Search With Search Bar" class="input input-bordered input-primary w-full max-w-xs" />
                 </div>
+                <div className='grid grid-cols-1 p-5 my-6 rounded-lg md:grid-cols-2 lg:grid-cols-5 bg-white gap-8'>
+                    {/* location   */}
+                    <div className='  border-r-2 '>
+                        <p className='text-gray-400 '>location</p>
+                        <p className='font-bold'>New York, USA</p>
+                    </div>
 
+                    <div className='  border-r-2 '>
+                        <p className='text-gray-400 '>Category</p>
+                        <select name='category' class="font-bold">
+                            {
+                                allCategories.map(category=> <option>{category}</option>)
+                            }                          
+                        </select>
+                    </div>
+                    <div className='  border-r-2 '>
+                        <p className='text-gray-400 '>Rate</p>
+                        <select name='rete' class="font-bold">
+                            <option>What is the best TV show</option>
+                        </select>
+                    </div>
+                    <div className='  border-r-2 '>
+                        <p className='text-gray-400 '>Price</p>
+                        <select name='price' class="font-bold">
+                            <option>What is the best TV show</option>
+                        </select>
+                    </div>
+                    <div>
+                        <input className='border-2 border-blue-700 font-semibold text-white ml-3 bg-blue-700 py-2 px-6 hover:bg-white hover:text-blue-700 rounded-lg' type="submit" value="Search" />
+                    </div>
+                </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 rounded-lg'>
+            {
+                cloths.map(cloth=><Cloth key={cloth.id} cloth={cloth} />)
+            }
+            </div>
             </div>
         </div>
     );
